e2ecfa99ece08d3701109feb844d937d41f385e0
Piyush Garg <piyushgarg.dev@gmail.com>
2024-03-04 21:53:28 +0530

    🎉 Begin Project

---
+++ script.js
@@ -0,0 +1,95 @@
+class ChatDOMService {
+    constructor() {
+        this.container = document.getElementById("chat-container");
+        this.__data = [{ id: "1", text: "Hey There", parentId: null }];
+
+        this.renderDOM();
+    }
+
+    /**
+     *
+     * @param {*} id
+     */
+    getParentById(id) {
+        return this.__data.find((e) => e.id === id);
+    }
+
+    /**
+     *
+     * @param {string | null} id
+     * @returns
+     */
+    getCommentsByParentId(id = null) {
+        return this.__data.filter((e) => e.parentId === id);
+    }
+
+    /**
+     *
+     * @param {string | null} parentId
+     */
+    handleAddReply(parentId = null) {
+        const parent = this.getParentById(parentId);
+        const reply = prompt(`Replying to ${parent.text}`);
+        this.__data.push({ id: Date.now().toString(), text: reply, parentId });
+        this.renderDOM();
+    }
+
+    /**
+     *
+     * @param {DocumentFragment | null} fragment
+     * @param {string | null} parentId
+     */
+    generateDataTree(fragment = null, parentId = null) {
+        if (!fragment) fragment = document.createDocumentFragment();
+
+        const nodes = this.getCommentsByParentId(parentId);
+
+        for (const node of nodes) {
+            const parentNode = document.createElement("div");
+
+            if (parentId) {
+                const styleNode = document.createElement("div");
+                parentNode.style.position = "relative";
+
+                parentNode.style.marginLeft = `20px`;
+                parentNode.style.paddingLeft = `20px`;
+
+                styleNode.classList.add("child-reply");
+                parentNode.appendChild(styleNode);
+            }
+
+            const addReplyButton = document.createElement("button");
+            addReplyButton.innerText = "Reply";
+
+            addReplyButton.addEventListener("click", (e) => {
+                this.handleAddReply(node.id);
+            });
+
+            const container = document.createElement("div");
+            container.classList.add("message-container");
+            const titleNode = document.createElement("p");
+
+            const textNode = document.createTextNode(node.text);
+            titleNode.appendChild(textNode);
+
+            container.appendChild(titleNode);
+            container.appendChild(addReplyButton);
+
+            parentNode.appendChild(container);
+
+            const childFragment = document.createDocumentFragment();
+            parentNode.appendChild(this.generateDataTree(childFragment, node.id));
+            fragment.appendChild(parentNode);
+        }
+
+        return fragment;
+    }
+
+    renderDOM() {
+        const renderedDOMFragment = this.generateDataTree();
+        this.container.innerHTML = "";
+        this.container.appendChild(renderedDOMFragment);
+    }
+}
+
+const service = new ChatDOMService();
